7633b204a552dceb5b43c0a9ac8a8e788ded0c96
William Lesniak <wlesnia1@gmail.com>
2025-06-26 17:08:32 -0400

    switching to HashRouter

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes } from "react-router-dom";
 import CallbackPage from './CallbackPage';
 import Login from './Login';
 
 function AppRouter() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="jammming/" element={<Login />} />
         <Route path="jammming/callback" element={<CallbackPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
