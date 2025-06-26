cf2767fb13eb4c157d5182461de025d49887093c
William Lesniak <wlesnia1@gmail.com>
2025-06-26 16:35:27 -0400

    trying to make this work on GH pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
+  base: "jammming",
   server: {
     host: "67.242.161.78",
     port: 5173,
